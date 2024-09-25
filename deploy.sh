e974d5c68016e691c7d9a86f4659cc91375241c8
Hazem Safwat <HSafwat@moj.gov.sa>
2024-09-25 20:10:37 +0300

    change app name

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ if [ -n "$newVersion" ]
 then
     echo $newVersion
     sed -i "s/\(APP_VERSION=\).*/\1$newVersion/" .env
-    ssh pinkstaging2 "cd /var/www/node_apps/<APP_NAME> &&  sed -i 's/\(APP_VERSION=\).*/\1$newVersion/' .env"
+    ssh pinkstaging2 "cd /var/www/node_apps/user_profile &&  sed -i 's/\(APP_VERSION=\).*/\1$newVersion/' .env"
 fi
 
 git push
-ssh pinkstaging2 "cd /var/www/node_apps/<APP_NAME> && git pull origin master  && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart <APP_NAME>"
+ssh pinkstaging2 "cd /var/www/node_apps/user_profile && git pull origin master  && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart user_profile"
 
 
 
